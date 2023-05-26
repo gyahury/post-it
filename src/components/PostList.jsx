61f4fb58396583b6a830f0e6adc61fb9813192cd
gyahury <rudgns1243@naver.com>
2023-05-26 22:24:41 +0900

    chore : 모달창 종료 함수 추가

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,8 +6,13 @@ import Modal from "./Modal"
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredTitle, setEnteredTitle] = useState("");
+
+  function hideModalHandler(){
+    setModalIsVisible(false);
+}
 
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
@@ -18,14 +23,13 @@ function PostList() {
   }
   return (
     <>
-      <Modal>
+      <Modal onClose={hideModalHandler}>
         <NewPost
           onChangeBody={changeBodyHandler}
           onChangeTitle={changeTitleHandler}
         />
       </Modal>
       <ul className={classes.posts}>
-        <Post title={enteredTitle} body={enteredBody} />
         <Post title={enteredTitle} body={enteredBody} />
       </ul>
     </>
